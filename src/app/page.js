f97075d63922bc2838baf56c55cc0fd8a73dbaf2
Khanh Tran <khanhtran@Khanhs-MacBook-Pro.local>
2024-02-02 19:57:49 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,181 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
+function currencyFormat(num) {
+  return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
 export default function Home() {
+  const [chieuDai, setChieuDai] = useState(0);
+  const [chieuRong, setChieuRong] = useState(0);
+  const [loaiMong, setloaiMong] = useState("option1");
+  const [soLuongSan, setSoLuongSan] = useState(0);
+  const [loaiMai, setLoaiMai] = useState("maiTon");
+  const [loaiSan, setLoaiSan] = useState("sanCoMaiChe");
+  const [chieuDaiSan, setChieuDaiSan] = useState(0);
+  const [chieuRongSan, setChieuRongSan] = useState(0);
+  const donGia = 3950000;
+  const dienTich = chieuDai * chieuRong;
+  const dienTichSan = chieuDaiSan * chieuRongSan;
+  const value = currencyFormat(
+    dienTich *
+      ((loaiMong === "option1" ? 0.2 : 0.4) +
+        (loaiMai === "maiTon" ? 0.3 : 0.5)) +
+      dienTich * soLuongSan +
+      dienTichSan *
+        (loaiSan === "sanCoMaiChe" ? 1 : loaiSan === "sanKoMaiChe" ? 0.7 : 0)
+  );
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={styles.center}></div>
+      <div>chieu dai</div>
+      <input
+        value={chieuDai}
+        onChange={(value) => {
+          setChieuDai(value.target.value);
+        }}
+      />
+      <div>chieu rong</div>
+      <input
+        value={chieuRong}
+        onChange={(value) => {
+          setChieuRong(value.target.value);
+        }}
+      />
+      <div>Số lượng sàn</div>
+      <input
+        value={soLuongSan}
+        onChange={(value) => {
+          setSoLuongSan(value.target.value);
+        }}
+      />
+      <div
+        onChange={(e) => {
+          setloaiMong(e.target.value);
+          console.log(e.target.value);
+        }}
+      >
+        Loại móng:
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option1"
+            checked={loaiMong === "option1"}
+          />
+          Móng đơn {dienTich * 0.2}m2
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option2"
+            checked={loaiMong === "option2"}
+          />
+          Móng băng {dienTich * 0.4}m2
+        </label>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        onChange={(e) => {
+          setLoaiMai(e.target.value);
+          console.log(e.target.value);
+        }}
+      >
+        Loại mái:
+        <label>
+          <input
+            type="radio"
+            name="fasdfasfd"
+            value="maiTon"
+            checked={loaiMai === "maiTon"}
+          />
+          Mái tôn {dienTich * 0.3}m2
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="fasdfasfd"
+            value="maiBetong"
+            checked={loaiMai === "maiBetong"}
+          />
+          Mái bê tông {dienTich * 0.5}m2
+        </label>
+      </div>
+      <div
+        onChange={(e) => {
+          setLoaiSan(e.target.value);
+          console.log(e.target.value);
+        }}
+      >
+        Loại sân:
+        <label>
+          <input
+            type="radio"
+            name="testsan"
+            value="sanCoMaiChe"
+            checked={loaiSan === "sanCoMaiChe"}
+          />
+          Sân có mái che {dienTichSan}m2
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="testsan"
+            value="sanKoMaiChe"
+            checked={loaiSan === "sanKoMaiChe"}
+          />
+          Sân không mái che {dienTichSan * 0.7}m2
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="testsan"
+            value=""
+            checked={loaiSan === ""}
+          />
+          không có sân
+        </label>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {loaiSan !== "" && (
+        <>
+          <div>chieu dai san</div>
+          <input
+            value={chieuDaiSan}
+            onChange={(value) => {
+              setChieuDaiSan(value.target.value);
+            }}
+          />
+          <div>chieu rong san</div>
+          <input
+            value={chieuRongSan}
+            onChange={(value) => {
+              setChieuRongSan(value.target.value);
+            }}
+          />
+        </>
+      )}
+      <button type="button">Click Me!</button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>don gia {currencyFormat(donGia)}d/m2</div>
+      <div>Dien tich: {value} m2</div>
+      <div>
+        Tien:
+        <h1>{currencyFormat(value * donGia)}</h1>
+      </div>
+      <div>Cát vàng đồng nai</div>
+      <div>Gạch tám quỳnh 8x8x18</div>
+      <div>Xi măng insse</div>
+      <div>
+        Thép việt nhật CB400
+        <br /> Betong thương phẩm (với điều kiện hẻm rộng)
+        <br />
+        Ống thoát nước Bình Minh Chống thấm kova ct11a
+        <br />
+        Ống cứng Dây điện cadivi Ống cấp và nóng lạnh ppr BÌnh Minh Đá Xanh hoặc
+        xám đồng nai
       </div>
     </main>
   );
